ba41000d29ab72806c9cdff13ad558a64b2d9655
YuriySamusew <6177385@gmail.com>
2023-04-20 01:06:09 +0300

    install mongoose and deploy db

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
+
+const DB_HOST =
+	"mongodb+srv://Samusev:SOeQfiJieJf0Qjrn@cluster0.bjowjll.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 // SOeQfiJieJf0Qjrn
 
-app.listen(3000, () => {
-	console.log("Server running. Use our API on port: 3000");
-});
+mongoose
+	.connect(DB_HOST)
+	.then(() =>
+		app.listen(3000, () => {
+			console.log("Server running. Use our API on port: 3000");
+		})
+	)
+	.catch((error) => console.log(error.message));
+
+// app.listen(3000, () => {
+// 	console.log("Server running. Use our API on port: 3000");
+// });
